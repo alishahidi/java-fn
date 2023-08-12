d12dad32eaa5cfa2c0cc9618d48b2f40fd4fc29d
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-12 14:04:07 +0330

    add ParallelStream example

--- src/main/java/org/alishahidi/streams/_ParallelStream.java
+++ src/main/java/org/alishahidi/streams/_ParallelStream.java
@@ -1,4 +1,24 @@
 package org.alishahidi.streams;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
 public class _ParallelStream {
+    public static void main(String[] args) {
+        long start = 0;
+        long end = 0;
+
+        start = System.currentTimeMillis();
+        IntStream.range(0, 100)
+                .forEach(System.out::println);
+        end = System.currentTimeMillis();
+        System.out.println("Plain stream: " + (end - start) + " ms");
+
+        start = System.currentTimeMillis();
+        IntStream.range(0, 100)
+                .parallel()
+                .forEach(System.out::println);
+        end = System.currentTimeMillis();
+        System.out.println("Parallel stream: " + (end - start) + " ms");
+    }
 }
